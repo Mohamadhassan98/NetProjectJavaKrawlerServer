318fa9f44dd42d66fdf3c50e74dbc27a6af56e26
HosseinAsadi76 <49914846+HosseinAsadi76@users.noreply.github.com>
2020-01-17 19:25:56 +0330

    complete crawl form

---
+++ src/utils/StaticAttributes.java
@@ -0,0 +1,7 @@
+package utils;
+
+public class StaticAttributes {
+    public static final String baseUrl = "http://lms.ui.ac.ir/";
+    public static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36";
+
+}
